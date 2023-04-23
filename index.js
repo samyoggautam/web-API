f2e06b23949db99a56786ee70464b50a14cdd991
samyog gautam <samyoggautam412@gmail.com>
2023-04-23 11:30:31 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+function primeFactors(num) {
+
+  let factors = [];
+
+  let divisor = 2;
+
+
+
+
+  while (num >= 2) {
+
+    if (num % divisor == 0) {
+
+      factors.push(divisor);
+
+      num /= divisor;
+
+    } else {
+
+      divisor++;
+
+    }
+
+  }
+
+
+
+
+  return factors;
+
+}
